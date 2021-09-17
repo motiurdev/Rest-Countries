aed0bea2ea838c5b375210ff72c80cba56a3ef37
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-09-17 23:00:36 +0600

    add country property

--- js/countries.js
+++ js/countries.js
@@ -5,17 +5,23 @@ const loadData = () => {
 }
 
 const showData = countries => {
+    console.log(countries[0]);
     const container = document.getElementById("countries")
     const country = countries.map(pd => getHtmlCount(pd))
     container.innerHTML = country.join(" ")
 }
 
 const getHtmlCount = data => {
+    const { name, capital, flag, topLevelDomain, population } = data;
     return `
     <div class="country">
-        <h2>Name: ${data.name}</h2>
-        <p>Capital: ${data.capital}</p>
-        <img src="${data.flag}" alt="">
+    <img src="${flag}" alt="">
+        <h2>Name: ${name}</h2>
+        <p><b>Capital:</b> ${capital}</p>
+        <p><b>Language:</b> ${data.languages[0].name}</p>
+        <p><b>Currency:</b> ${data.currencies[0].name}(${data.currencies[0].symbol})</p>
+        <p><b>Top Level Domain:</b> ${topLevelDomain}</p>
+        <p><b>Population:</b> ${population}</p>
     </div>
     `
 }
